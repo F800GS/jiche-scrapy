aa498ee7c0e97ed5f4efcbf2f226ec349eec211d
F800GS <bengcaca201610@foxmail.com>
2016-09-26 11:55:04 +0800

    Update items.py

--- jiche/items.py
+++ jiche/items.py
@@ -1,1 +1,20 @@
 
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class JicheItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    # pass
+    models = scrapy.Field()
+    capacity = scrapy.Field()
+    price = scrapy.Field()
+    payload = scrapy.Field()
+    link = scrapy.Field()
